b44781d4abfef0f2d603fb7cd26642fa04e4c0d8
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2022-11-08 17:07:03 -0700

    Basics for the assignment

---
+++ src/employee.java
@@ -0,0 +1,17 @@
+public class employee  {
+    public static void main(String[] args) throws Exception {
+        int id;
+        String firstName;
+        String lastName;
+        int hireYear;
+        
+    }
+
+    public void PrintPeopleDetails() {
+        
+    }
+
+    public void PrintEmployees() {
+        
+    }
+}
