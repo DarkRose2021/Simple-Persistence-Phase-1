5183ec5616df80ea103922778ccd48498e7d1218
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2022-11-11 15:56:36 -0700

    Finished the DeleteEmployee

--- src/edu/neumont/dbt/phase1/App.java
+++ src/edu/neumont/dbt/phase1/App.java
@@ -2,15 +2,18 @@ package edu.neumont.dbt.phase1;
 
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        PrintEmployees("C:\\aNeumont\\Year 2\\Q1\\Databases II\\Simple Persistence\\Assignment 1 - data-1\\people\\simple");
+        // PrintEmployees("C:\\aNeumont\\Year 2\\Q1\\Databases II\\Simple Persistence\\Assignment 1 - data-1\\people\\long");
+        // DeleteEmployee("C:\\aNeumont\\Year 2\\Q1\\Databases II\\Simple Persistence\\Assignment 1 - data-1\\people\\long", 23);
     }
     public static void PrintPeopleDetails(String path) {
         File directory = new File(path);
@@ -78,26 +81,33 @@ public class App {
             // Close file
     }
 
-    public void AddEmployee(String path, int id, String firstName, String lastName, int hireYear) {
+    public static void AddEmployee(String path, int id, String firstName, String lastName, int hireYear) {
         File directory = new File(path);
-        File[] employeeListing = directory.listFiles();
+        directory.mkdir();
+        String hash = String.valueOf(id);
+        String fileName = hash+ ".txt";
+        // File newEmployee new File(directory, fileName);
+        // newEmployee.createNewFile();
     }
 
-    public void DeleteEmployee(String path, int id) {
-        File directory = new File(path);
-        File[] employeeListing = directory.listFiles();
+    public static void DeleteEmployee(String path, int id) {
+        File file = new File(path, id+".txt");
+        if(file.delete()){
+            System.out.println(file.getName()+ " deleted");
+        }else{
+            System.out.println("File not found");
+        }
     }
 
-    public void UpdateEmployee(String path, int id) {
-        File directory = new File(path);
-        File[] employeeListing = directory.listFiles();
-    }
-
-    public void SerializeAllEmployees(String path) {
+    public static void UpdateEmployee(String path, int id) {
         File directory = new File(path);
     }
 
-    public void GetSerializeAllEmployees(String path, int id) {
+    public static void SerializeAllEmployees(String path) {
+        File directory = new File(path);
+    }
+
+    public static void GetSerializeAllEmployees(String path, int id) {
         File directory = new File(path);
     }
 }
